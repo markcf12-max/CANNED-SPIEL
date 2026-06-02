6577763ccf20c0dc772a63bbd13d09979f8c6a74
markcf12-max <markcf12@gmail.com>
2026-06-02 08:27:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -386,6 +386,31 @@ document.getElementById("searchInput").addEventListener("input", (e) => {
 });
 
 /* =========================
+   SCROLL TOP BUTTON
+========================= */
+
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+
+  if (window.scrollY > 300) {
+    scrollTopBtn.classList.add("show");
+  } else {
+    scrollTopBtn.classList.remove("show");
+  }
+
+});
+
+scrollTopBtn.addEventListener("click", () => {
+
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+
+});
+
+/* =========================
    INIT
 ========================= */
 
